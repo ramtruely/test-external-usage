f6c99a9c98ac5b412a9cbc00e5fe37b5e9184e6c
ramtruely <ramtruely6@gmail.com>
2025-12-06 18:38:51 +0530

    Update loader.groovy

--- vars/loader.groovy
+++ vars/loader.groovy
@@ -1,27 +1,8 @@
-// vars/loader.groovy
-// sandbox-safe loader that uses load(...) on src files and returns instances
-
-def get(String shortName, Object... args) {
-    // map simple names to file paths (adjust if you use other naming)
-    def map = [
-        'Utility': 'src/org/my/example/MyHelper.groovy',
-        'Helper' : 'src/org/my/example/MyUtility.groovy'
-    ]
-
-    def path = map[shortName]
-    if (!path) {
-        error "Unknown class ${shortName}"
-    }
-
-    // load returns the script object (because each src returns this)
-    def scriptObj = load(path)
-
-    // call its factory 'create' with args
-    if (args?.length > 0) {
-        return scriptObj.create(*args)
-    } else {
-        return scriptObj.create()
-    }
+def loadHelper(script) {
+    return new helper.MyHelper(script)
 }
 
+def loadUtility(script) {
+    return new util.MyUtility(script)
+}
 return this
