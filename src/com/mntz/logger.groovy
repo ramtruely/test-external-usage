cd7cef20571b026871c4654ded3423facfde3ec9
ramtruely <ramtruely6@gmail.com>
2025-12-06 15:37:59 +0530

    Create logger.groovy

---
+++ src/com/mntz/logger.groovy
@@ -0,0 +1,12 @@
+package com.mntz
+
+class Utility {
+
+    def echoMsg(String msg) {
+        println "Utility instance says: ${msg}"
+    }
+
+    def addNumbers(int a, int b) {
+        return a + b
+    }
+}
