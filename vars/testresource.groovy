88d1e3076ded4ad89fc9deda34139515ac3e6e12
ramtruely <ramtruely6@gmail.com>
2025-12-06 17:17:53 +0530

    Update testresource.groovy

--- vars/testresource.groovy
+++ vars/testresource.groovy
@@ -1,1 +1,6 @@
-Hello from resources!
+def call() {
+    // Read resource file located in resources/sample.txt
+    def content = libraryResource 'sample.txt'
+    echo "Resource Content => ${content}"
+}
+return this
