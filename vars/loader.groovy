ccaa0df11c239d607bb8262ca1783c7709b71a93
ramtruely <ramtruely6@gmail.com>
2025-12-06 16:19:23 +0530

    Update loader.groovy

--- vars/loader.groovy
+++ vars/loader.groovy
@@ -1,10 +1,9 @@
-// sandbox-safe loader.groovy
 def loadClasses() {
     def classesMap = [:]
 
     // Instantiate classes directly
-    classesMap['Utility'] = new com.mntz.Utility("DEV")
-    classesMap['Helper']  = new com.mntz.Helper("Ram")
+    classesMap['Utility'] = new Utility("DEV")
+    classesMap['Helper']  = new Helper("Ram")
 
     return classesMap
 }
