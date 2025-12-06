88708ef57825d7fc1bedb4d84f20d61630220a84
ramtruely <ramtruely6@gmail.com>
2025-12-06 17:49:57 +0530

    Update testsh.groovy

--- vars/testsh.groovy
+++ vars/testsh.groovy
@@ -2,14 +2,13 @@ def runTestSh() {
     echo "Running NON shared lib test.sh …"
 
     def content = readFile 'resources/test.sh'
-
     writeFile file: 'test.sh', text: content
 
     if (isUnix()) {
         sh 'chmod +x test.sh'
         sh './test.sh'
     } else {
-        bat 'test.sh'
+        bat 'bash test.sh'
     }
 }
 
