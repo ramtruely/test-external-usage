cfc27f68c5e8b01169532e2db51deaea86b3b5aa
ramtruely <ramtruely6@gmail.com>
2025-12-06 17:31:30 +0530

    Delete vars/loader.groovy

--- vars/loader.groovy
+++
@@ -1,9 +0,0 @@
-def loadClasses() {
-    def classesMap = [:]
-
-    // Instantiate classes directly
-    classesMap['Utility'] = new Utility("DEV")
-    classesMap['Helper']  = new Helper("Ram")
-
-    return classesMap
-}
